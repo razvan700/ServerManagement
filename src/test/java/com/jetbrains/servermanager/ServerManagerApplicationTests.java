eefcc331154be2289013f58a989bba0e08a21f70
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-04 10:21:24 +0300

    Initial commit

---
+++ src/test/java/com/jetbrains/servermanager/ServerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.servermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
