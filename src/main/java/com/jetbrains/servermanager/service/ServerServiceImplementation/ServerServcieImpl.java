bb6a8e790325969f00f89e051471746c56ff9a8e
razvan700 <110129667+razvan700@users.noreply.github.com>
2025-04-05 02:03:23 +0300

    Instantiated repository in ServerServcieImpl

--- src/main/java/com/jetbrains/servermanager/service/ServerServiceImplementation/ServerServcieImpl.java
+++ src/main/java/com/jetbrains/servermanager/service/ServerServiceImplementation/ServerServcieImpl.java
@@ -1,11 +1,22 @@
 package com.jetbrains.servermanager.service.ServerServiceImplementation;
 
 import com.jetbrains.servermanager.model.Server;
+import com.jetbrains.servermanager.repository.ServerRepository;
 import com.jetbrains.servermanager.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServcieImpl implements ServerService {
+
+    private final ServerRepository serverRepository;
     @Override
     public Server create(Server server){
         return new Server();
